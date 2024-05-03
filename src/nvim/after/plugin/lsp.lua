02775a9736a2423ea5aeae5b52cbc6e032e7dd89
s4ms1ll1us <tmn@posteo.de>
2024-05-03 21:32:53 +0200

    Add packages for auto completion

--- src/nvim/after/plugin/lsp.lua
+++ src/nvim/after/plugin/lsp.lua
@@ -1,3 +1,6 @@
+local lspzero = require('lsp-zero')
+lspzero.preset('recommended')
+
 require('mason').setup()
 require('mason-lspconfig').setup({
     ensure_installed = {
@@ -8,8 +11,15 @@ require('mason-lspconfig').setup({
     }
 })
 
-local lsp = require('lspconfig')
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lspzero.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm(),
+})
 
+local lsp = require('lspconfig')
 lsp.lua_ls.setup {}
 lsp.pyright.setup {}
 lsp.pylsp.setup {}
--- src/nvim/lua/s4ms1ll1us/packer.lua
+++ src/nvim/lua/s4ms1ll1us/packer.lua
@@ -26,9 +26,22 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
+            -- LSP Support
             {'williamboman/mason.nvim'},
             {'williamboman/mason-lspconfig.nvim'},
             {'neovim/nvim-lspconfig'},
+
+            -- Auto Completion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
         }
     }
 end)

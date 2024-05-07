48d5f9e7e882167bc769d8f3ec2b0e7933587431
s4ms1ll1us <tmn@posteo.de>
2024-05-07 08:59:08 +0200

    Set tab width

--- src/nvim/lua/config/options.lua
+++ src/nvim/lua/config/options.lua
@@ -2,6 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true

2690e225986f01e08c88990b69ceccbea3e0b64d
s4ms1ll1us <tmn@posteo.de>
2024-05-07 15:20:34 +0200

    Disabled some plugins I do not need
    Also updated all plugins

--- src/nvim/lua/plugins/disabled.lua
+++ src/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,7 @@
 return {
     { "nvimdev/dashboard-nvim", enabled = false },
+    { "folke/which-key.nvim", enabled = false },
+    { "echasnovski/mini.ai.nvim", enabled = false },
+    { "ellisonleao/gruvbox.nvim", enabled = false },
+    { "lewis6991/gitsigns.nvim", enabled = false },
 }

37c1aa365892b9db4eca7edc37b9296addddf3ee
s4ms1ll1us <tmn@posteo.de>
2024-05-08 13:19:05 +0200

    Fix disabling mini.ai

--- src/nvim/lua/plugins/disabled.lua
+++ src/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
     { "nvimdev/dashboard-nvim", enabled = false },
     { "folke/which-key.nvim", enabled = false },
-    { "echasnovski/mini.ai.nvim", enabled = false },
+    { "echasnovski/mini.ai", enabled = false },
     { "ellisonleao/gruvbox.nvim", enabled = false },
     { "lewis6991/gitsigns.nvim", enabled = false },
     { "nvim-lualine/lualine.nvim", enabled = false },

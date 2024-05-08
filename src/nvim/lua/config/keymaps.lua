a9ffb461c3ec9301ca07edacb6302299e5476922
s4ms1ll1us <tmn@posteo.de>
2024-05-08 13:30:52 +0200

    Adding some fantastic remaps

--- src/nvim/lua/config/keymaps.lua
+++ src/nvim/lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- Move selections properly
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor position if appending lines from underneath
+keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in the middle if half way jumping
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Same for search terms
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- Cut and paste without loosing the current paste buffer
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- Pasting buffers
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Search and replace
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])

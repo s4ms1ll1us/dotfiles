8563bde1081b4ac8f367d3c0a3b401a9c8163ce4
s4ms1ll1us <tmn@posteo.de>
2024-05-08 12:37:27 +0200

    Disable not needed plugins

--- src/nvim/lua/plugins/disabled.lua
+++ src/nvim/lua/plugins/disabled.lua
@@ -4,4 +4,8 @@ return {
     { "echasnovski/mini.ai.nvim", enabled = false },
     { "ellisonleao/gruvbox.nvim", enabled = false },
     { "lewis6991/gitsigns.nvim", enabled = false },
+    { "nvim-lualine/lualine.nvim", enabled = false },
+    { "folke/noice.nvim", enabled = false },
+    { "rcarriga/nvim-notify", enabled = false },
+    { "nvim-tree/nvim-web-devicons", enabled = false },
 }
